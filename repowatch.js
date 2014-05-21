9845598c3ade7d13a7fb2a948ad2dbf2ef43c948
Joseph Jung <jungyue@gmail.com>
2014-05-21 16:57:39 -0500

    directory structure and stuff

--- repowatch.js
+++ repowatch.js
@@ -1,18 +1,37 @@
-var chokidar = require('chokidar');
-var commander = require('commander');
-var versionInfo = require('./package.json')
-
+var versionInfo = require('./package.json'),
+	async = require('async'),
+	chokidar = require('chokidar'),
+ 	commander = require('commander'),
+ 	path = require('path'),
+ 	fs = require('fs');
+ 	
 commander
 	.usage("repowatch [options] (default repo is taken from the current folder)")
 	.version(versionInfo.version)
-	.option('-r, --repo [repo]', 'Watch the specified repository/folder.', __dirname)
+	.option('-r, --repo [repo]', 'The name of the repo to be watched.', __dirname)
 	.parse(process.argv);
 
-console.log('Watching repo at %s ...', commander.repo);
+// Setup a mongo database object and connect to the server
+var mongodb = require('./lib/db').db;
 
-// TODO: sanitize folder input.
-chokidar.watch(commander.repo, {ignored: /[\/\\]\./, persistent: true}).on('all', reactToChange);
+// bootstrap models
+var modelsPath = path.join(__dirname + "/lib/models")
+fs.readdirSync(modelsPath).forEach(function (file) {
+  require(modelsPath + '/' + file)
+})
+
+console.log('Watching repo at %s ...', commander.repo);
+chokidar.watch(commander.repo, {ignored: /node_modules/, persistent: true}).on('all', reactToChange);
+
 
 function reactToChange(event, path, stats) {
+	
 	console.log(event);
+
+	// Build new collection
+
 }
+
+
+
+	
