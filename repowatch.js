4ae2cf0b44dfc7d1157f79682b728334253fb60b
Joseph Jung <jungyue@gmail.com>
2014-05-21 21:33:22 -0500

    Add more stuff

--- repowatch.js
+++ repowatch.js
@@ -1,33 +1,56 @@
 var versionInfo = require('./package.json'),
-	async = require('async'),
 	chokidar = require('chokidar'),
  	commander = require('commander'),
- 	path = require('path'),
- 	fs = require('fs');
- 	
+ 	mongoose = require('mongoose'),
+ 	mongodb = require('./lib/db').db;
+
+require('./lib/models/repo.js');
+var RepoModel = mongoose.model("Repo");
+
 commander
 	.usage("repowatch [options] (default repo is taken from the current folder)")
 	.version(versionInfo.version)
-	.option('-r, --repo [repo]', 'The name of the repo to be watched.', __dirname)
+	.option('-r, --repo [repo]', 'The name of the repo to be watched.', null)
 	.parse(process.argv);
 
-// Setup a mongo database object and connect to the server
-var mongodb = require('./lib/db').db;
+// require the repo option
+if (!commander.repo) { 
+	console.log("repo name must be specified.  user -r [repoName];");
+	process.kill(process.pid);
+}
 
-// bootstrap models
-var modelsPath = path.join(__dirname + "/lib/models")
-fs.readdirSync(modelsPath).forEach(function (file) {
-  require(modelsPath + '/' + file)
-})
+// the _id of the document we are dealing with.
+var workingRepo = null;
 
 // start watching files when we've connected to the mongo db.
 mongodb.connection.once("open", function() {
-	console.log('Watching repo at %s ...', commander.repo);
-	chokidar.watch(commander.repo, {ignored: /node_modules/, persistent: true}).on('all', reactToChange);
-})
 
+	// check for existence of a repo by this name
+	RepoModel.count({"name": commander.repo}, function(err, count) {
+		
+		if (err) {console.log(err); return }
+
+		// if doesn't exist, create a new document for it.  or else find the existing one and get its id.
+		if (count==0) {
+			currentRepo = new RepoModel({name: commander.repo })
+			currentRepo.save(startWatcher);
+		} else {
+			RepoModel.findOne({"name": commander.repo}, startWatcher)
+		}
+	});
+	
+})	
+
+// executed after connection to db and collection for the repo is made/found
+function startWatcher(err, repo) {
+	workingRepo = repo._id;
+	console.log('Watching repo %s ...', commander.repo);
+	chokidar.watch(__dirname, {ignored: /node_modules/, persistent: true}).on('all', reactToChange);
+}
+
+// executed anytime a new file has been changed/created/deleted
 function reactToChange(event, path, stats) {
-	// console.log(event);
+	// console.log(workingRepo)
 }
 
 
